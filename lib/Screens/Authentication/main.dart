36702810f53fe3ff96b07296ea94bd86fd90f828
mzimkhulu <mzincube161@gmail.com>
2020-01-28 23:51:20 +0000

    added screens

---
+++ lib/Screens/Authentication/main.dart
@@ -0,0 +1,210 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:hair_cos/Models/User.dart';
+import 'package:hair_cos/Screens/Authentication/signup.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+import 'package:hair_cos/Services/Database.dart';
+import 'package:hair_cos/StateContainers/LoginStateContainer.dart';
+import '../UserHome/NavBar.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(
+    StateContainer(
+      child: MyApp(),
+      database: DatabaseServices(User(uid: "hello")),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: loginView(),
+      ),
+    );
+  }
+}
+
+class loginView extends StatefulWidget {
+  _loginView createState() => _loginView();
+}
+
+class _loginView extends State<loginView> {
+  final formKey = GlobalKey<FormState>();
+  String email, password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(shape: BoxShape.circle),
+              child: SvgPicture.asset(
+                'asserts/logo.svg',
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.75,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              child: loginContent(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class loginContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'Enter your email',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              hintText: 'Enter your password',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 10),
+          child: loginButtons("LOGIN", Colors.blue, Colors.white),
+        ),
+        Center(
+          child: Text(
+            "- or -",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: loginButtons("Login with google", Colors.red, Colors.white),
+        ),
+        loginButtons("Login with facebook", Colors.indigo, Colors.white),
+        Center(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return signup();
+                    },
+                  ),
+                );
+              },
+              child: Text("No Account? Sign up"),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class loginButtons extends StatelessWidget {
+  final Color background;
+  final String txt;
+  final Color textColor;
+
+  loginButtons(this.txt, this.background, this.textColor);
+
+  @override
+  Widget build(BuildContext context) {
+    final container = StateContainer.of(context);
+    return RaisedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Logging in"),
+              content: Text("Press continue if you would like to login"),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Close"),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    container.updateUser(User(uid: "baokdnyeh73bh84hks"));
+                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+                      builder: (context) {
+                        return navBar();
+                      },
+                    ), (e) => false);
+                  },
+                  child: Text("Continue"),
+                )
+              ],
+            );
+          },
+        );
+      },
+      color: background,
+      splashColor: Colors.grey,
+      child: Container(
+        height: 50,
+        child: Center(
+          child: Text(
+            txt,
+            style: TextStyle(color: textColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
+// MediaQuery.of(context).size.width;
---
+++ lib/Screens/Authentication/signup.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hair_cos/Models/User.dart';
+import 'package:hair_cos/Screens/ShopHome/ShopOwnerHome.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hair_cos/Services/Database.dart';
+import 'package:hair_cos/StateContainers/LoginStateContainer.dart';
+
+class signup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(shape: BoxShape.circle),
+              child: SvgPicture.asset(
+                'asserts/logoReverse.svg',
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                fit: BoxFit.fill,
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                padding: const EdgeInsets.all(30),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.75,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                ),
+                child: signupContent(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class signupContent extends StatefulWidget {
+  _signupContent createState() => _signupContent();
+}
+
+class _signupContent extends State<signupContent> {
+  bool checkValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Name',
+              hintText: 'Enter your name',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'Enter your email',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              hintText: 'Enter your password',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Confirm Password',
+              hintText: 'Confirm your password',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+            ),
+          ),
+        ),
+        Row(children: <Widget>[
+          Checkbox(
+              value: checkValue,
+              onChanged: (bool value) {
+                setState(() {
+                  checkValue = value;
+                });
+              }),
+          Text("Do you want an account as a shop owner")
+        ]),
+        Text(
+          "By pressing sign up you agree to the terms and conditions",
+          style: TextStyle(color: Colors.indigo),
+        ),
+        SignInButtons("Sign up", Colors.blue, Colors.white),
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+            child: Center(
+              child: Text(
+                "Have an account? Sign in",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SignInButtons extends StatelessWidget {
+  final Color background;
+  final String txt;
+  final Color textColor;
+
+  SignInButtons(this.txt, this.background, this.textColor);
+
+  @override
+  Widget build(BuildContext context) {
+    final container = StateContainer.of(context);
+    return RaisedButton(
+      onPressed: () {
+        container.updateUser(User(uid: "bagojvkdnyeh73bh84hks"));
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return ShopOwnerHome();
+            },
+          ),
+          (e) => false,
+        );
+      },
+      color: background,
+      splashColor: Colors.grey,
+      child: Container(
+        height: 50,
+        child: Center(
+          child: Text(
+            txt,
+            style: TextStyle(color: textColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
