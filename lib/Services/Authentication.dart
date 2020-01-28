3c3fc87ca4d961e9822629b03177908ae84ae122
mzimkhulu <mzincube161@gmail.com>
2020-01-28 15:49:54 +0000

    authentication comments added

---
+++ lib/Services/Authentication.dart
@@ -0,0 +1,21 @@
+class AuthenticationServices {
+  // sign in anonymously
+
+  // sign in with email
+
+  // register with email
+
+  // change password
+
+  // shop sign in
+
+  // shop register
+
+  // sign in with google
+
+  // sign in with facebook
+
+  // register with google
+
+  // register with facebook
+}
---
+++ lib/Services/Functions.dart
@@ -0,0 +1,4 @@
+
+class FunctionsServices{
+  // implement later
+}
