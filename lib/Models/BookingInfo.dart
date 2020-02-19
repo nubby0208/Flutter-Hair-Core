102630598162b22247b6401ac45fcb3e7e4b2c24
K_monkey <mykhailovirii@gmail.com>
2020-02-19 10:53:31 +1000

    empty

--- lib/Models/BookingInfo.dart
+++
@@ -1,9 +0,0 @@
-class BookingInfo {
-  String profileImage = "";
-  String types = "";
-  String name = "";
-  String time = "";
-  String date = "";
-  String price = "";
-  String duration = "";
-}
--- lib/Services/Functions.dart
+++
@@ -1,4 +0,0 @@
-
-class FunctionsServices{
-  // implement later
-}
