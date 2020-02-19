472bc5308042cb6b6b2c5e131518fd16f350382a
monkey1992719 <mykhailovirii@gmail.com>
2020-02-19 14:45:57 -0800

    user module finish

--- lib/Screens/Authentication/login.dart
+++ lib/Screens/Authentication/login.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hair_cos/CustomViews/CustomButton.dart';
 import 'package:hair_cos/CustomViews/Loading.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hair_cos/Models/User.dart';
+import 'package:hair_cos/Screens/UserHome/Home.dart';
 import 'package:hair_cos/Services/Authentication.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginView extends StatelessWidget {
   final Auth auth;
+
   LoginView({this.auth});
   @override
   Widget build(BuildContext context) {
@@ -59,6 +65,10 @@ class LoginContent extends StatefulWidget {
 
 class _LoginContentState extends State<LoginContent> {
   bool loading = false;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final userEmail = TextEditingController();
+  final userPassword = TextEditingController();
+  bool onSave = false;
 
   @override
   void initState() {
@@ -68,73 +78,94 @@ class _LoginContentState extends State<LoginContent> {
 
   @override
   Widget build(BuildContext context) {
-    return loading
-        ? Loading()
-        : ListView(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 30, 5),
-                child: CustomButton.roundedButton(context, txt: "Login".toUpperCase()),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Wrap(
-                    children: <Widget>[
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          FontAwesomeIcons.facebookSquare,
-                          color: Colors.indigo,
-                          size: 40,
+    return Scaffold(
+      body: loading
+          ? Loading()
+          : ModalProgressHUD(
+              inAsyncCall: onSave,
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                    child: TextFormField(
+                      controller: userEmail,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
                         ),
                       ),
-                      IconButton(
-                        onPressed: () async {
-                          //googleLogin(context);
-                        },
-                        icon: Icon(
-                          FontAwesomeIcons.google,
-                          color: Colors.red,
-                          size: 40,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                    child: TextFormField(
+                      controller: userPassword,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                  child: InkWell(
-                    onTap: () async {
-                      load(true);
-                      /* dynamic result =
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(30, 0, 30, 5),
+                    child: CustomButton.roundedButton(context,
+                        txt: "Login".toUpperCase(), onPress: () {
+                      save(true);
+
+                      _logIn();
+                    }),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (_) => forgotPass(context));
+                      },
+                      child: Text('Forgot Password'),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Wrap(
+                        children: <Widget>[
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              FontAwesomeIcons.facebookSquare,
+                              color: Colors.indigo,
+                              size: 40,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              //googleLogin(context);
+                            },
+                            icon: Icon(
+                              FontAwesomeIcons.google,
+                              color: Colors.red,
+                              size: 40,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      child: InkWell(
+                        onTap: () async {
+                          load(true);
+                          /* dynamic result =
                           await widget.auth.signInAnon();
                       if (result == null) {
                         load(false);
@@ -146,13 +177,14 @@ class _LoginContentState extends State<LoginContent> {
                         
                         Navigator.of(context).pop();
                       } */
-                    },
-                    child: Text("Continue as Guest"),
+                        },
+                        child: Text("Continue as Guest"),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ],
-          );
+                ],
+              )),
+    );
   }
 
   void errorDialog(String message, BuildContext context) {
@@ -175,7 +207,200 @@ class _LoginContentState extends State<LoginContent> {
     );
   }
 
- /*  void googleLogin(context) async {
+  Widget successDialogue(BuildContext context) {
+    return Dialog(
+      child: Container(
+          padding: EdgeInsets.all(15.0),
+          height: MediaQuery.of(context).size.width / 2,
+          width: 300,
+          decoration: ShapeDecoration(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  'Reset link has been set to your email.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blueGrey, fontSize: 18),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue, width: 3),
+                    borderRadius: BorderRadius.circular(5)),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    '   OK   ',
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
+  Widget forgotPass(BuildContext context) {
+    return Dialog(
+        child: Container(
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(15.0),
+            height: MediaQuery.of(context).size.width / 1.2,
+            decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Enter the email address linked to your account',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.blueGrey, fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                  ),
+                  width: 220,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue, width: 3),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.blueGrey),
+                    cursorColor: Colors.blue,
+                    textAlign: TextAlign.left,
+                    controller: userEmail,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter Email',
+                        hintStyle:
+                            TextStyle(color: Colors.blueGrey, fontSize: 18)),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(),
+                  height: 40,
+                  width: 130,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(10),
+                        topLeft: Radius.circular(10)),
+                    color: Colors.blueAccent,
+                  ),
+                  child: InkWell(
+                      onTap: () {
+                        if (userEmail.text == null || userEmail.text == '') {
+                          Fluttertoast.showToast(
+                            msg: 'Please enter an email',
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.BOTTOM,
+                            timeInSecForIos: 1,
+                          );
+                        } else {
+                          FirebaseAuth.instance
+                              .sendPasswordResetEmail(email: userEmail.text);
+                          Navigator.of(context).pop();
+                          showDialog(
+                              context: context,
+                              builder: (_) => successDialogue(context));
+                        }
+                      },
+                      child: Text(
+                        'Send reset link',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )),
+                ),
+              ],
+            )));
+  }
+
+  void _logIn() async {
+    if (userEmail.text.isNotEmpty) {
+      try {
+        await _firebaseAuth
+            .signInWithEmailAndPassword(
+                email: userEmail.text, password: userPassword.text)
+            .then((onValue) async {
+          User.userData.userId = onValue.user.uid;
+          save(false);
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Home()));
+        });
+      } catch (signinError) {
+        if (signinError.toString().contains("Given String is empty or null")) {
+        } else if (signinError.toString().contains("ERROR_INVALID_EMAIL")) {
+          Fluttertoast.showToast(
+            msg: 'The email is invalid',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signinError);
+        } else if (signinError.toString().contains("ERROR_USER_NOT_FOUND")) {
+          Fluttertoast.showToast(
+            msg: 'No user found with this email',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signinError);
+        } else if (signinError.toString().contains("ERROR_WRONG_PASSWORD")) {
+          Fluttertoast.showToast(
+            msg: 'wrong password try again',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signinError);
+        } else if (signinError
+            .toString()
+            .contains("ERROR_EMAIL_ALREADY_IN_USE")) {
+          Fluttertoast.showToast(
+            msg: 'email entered is already in use.',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signinError);
+        } else {
+          print(signinError);
+        }
+      }
+    } else {
+      save(false);
+      Fluttertoast.showToast(
+          msg: 'Please enter an email.',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 1);
+    }
+    if (userPassword.text.isEmpty) {
+      save(false);
+      Fluttertoast.showToast(
+          msg: 'Please enter password.',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 1);
+    }
+  }
+
+  /*  void googleLogin(context) async {
     final container = StateContainer.of(context);
     load(true);
     dynamic result = await AuthenticationServices().testSignInWithGoogle();
@@ -228,6 +453,12 @@ class _LoginContentState extends State<LoginContent> {
   void load(bool state) {
     setState(() {
       loading = state;
+    });
+  }
+
+  void save(bool state) {
+    setState(() {
+      onSave = state;
     });
   }
 }
--- lib/Screens/Authentication/signup.dart
+++ lib/Screens/Authentication/signup.dart
@@ -1,10 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hair_cos/CustomViews/CustomButton.dart';
 import 'package:hair_cos/CustomViews/Loading.dart';
 import 'package:flutter_svg/svg.dart';
-
+import 'package:hair_cos/Models/User.dart';
+import 'package:hair_cos/Screens/Authentication/login.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class SignUp extends StatelessWidget {
   final bool shopSignUp;
@@ -41,7 +46,9 @@ class SignUp extends StatelessWidget {
                     topRight: Radius.circular(50),
                   ),
                 ),
-                child: SignUpContent(shopSignUp: shopSignUp,),
+                child: SignUpContent(
+                  shopSignUp: shopSignUp,
+                ),
               ),
             ),
           ],
@@ -54,92 +61,117 @@ class SignUp extends StatelessWidget {
 class SignUpContent extends StatefulWidget {
   final bool shopSignUp;
   SignUpContent({this.shopSignUp});
+
   _SignUpContent createState() => _SignUpContent();
 }
 
 class _SignUpContent extends State<SignUpContent> {
   bool loading = false;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final userEmail = TextEditingController();
+  final userPassword = TextEditingController();
+  final userConfirmPass = TextEditingController();
+  bool onSave = false;
   @override
   Widget build(BuildContext context) {
     return loading
         ? Loading()
-        : ListView(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-          child: TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              hintText: 'Enter your email',
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue),
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-          child: TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              hintText: 'Enter your password',
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue),
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-          child: TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Confirm Password',
-              hintText: 'Confirm your password',
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue),
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0,5,0,10),
-          child: Text(
-            "By pressing sign up you agree to the terms and conditions",
-            style: TextStyle(color: Colors.indigo),
-          ),
-        ),
-        CustomButton.roundedButton(context, txt: "Sign up".toUpperCase()),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Wrap(
+        : ModalProgressHUD(
+            inAsyncCall: onSave,
+            child: ListView(
               children: <Widget>[
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    FontAwesomeIcons.facebookSquare,
-                    color: Colors.indigo,
-                    size: 40,
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextFormField(
+                    controller: userEmail,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                    ),
                   ),
                 ),
-                IconButton(
-                  onPressed: () async {
-                    //googleLogin(context);
-                  },
-                  icon: Icon(
-                    FontAwesomeIcons.google,
-                    color: Colors.red,
-                    size: 40,
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextFormField(
+                    controller: userPassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextFormField(
+                    controller: userConfirmPass,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      hintText: 'Confirm your password',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
+                  child: Text(
+                    "By pressing sign up you agree to the terms and conditions",
+                    style: TextStyle(color: Colors.indigo),
+                  ),
+                ),
+                CustomButton.roundedButton(context,
+                    txt: "Sign up".toUpperCase(), onPress: () {
+                  if (userPassword.text != userConfirmPass.text) {
+                    Fluttertoast.showToast(
+                      msg: 'Password does not match ',
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIos: 1,
+                    );
+                  } else {
+                    save(true);
+
+                    _register();
+                  }
+                }),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Wrap(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            FontAwesomeIcons.facebookSquare,
+                            color: Colors.indigo,
+                            size: 40,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            //googleLogin(context);
+                          },
+                          icon: Icon(
+                            FontAwesomeIcons.google,
+                            color: Colors.red,
+                            size: 40,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
-            ),
-          ),
-        ),
-      ],
-    );
+            ));
   }
 
   /* void googleLogin(context) async {
@@ -181,6 +213,12 @@ class _SignUpContent extends State<SignUpContent> {
     }
   } */
 
+  void save(bool state) {
+    setState(() {
+      onSave = state;
+    });
+  }
+
   void errorDialog(String message, BuildContext context) {
     showDialog(
       context: context,
@@ -199,6 +237,73 @@ class _SignUpContent extends State<SignUpContent> {
         );
       },
     );
+  }
+
+  void _register() async {
+    if (userEmail.text.isNotEmpty) {
+      try {
+        FirebaseUser user = (await _firebaseAuth.createUserWithEmailAndPassword(
+                email: userEmail.text, password: userPassword.text))
+            .user;
+        print('${user.uid}');
+        if (user != null) {
+          Firestore.instance.collection('Users').document(user.uid).setData({
+            'Email': userEmail.text,
+            'profile_photo': "",
+          });
+          User.userData.userId = user.uid.toString();
+          FirebaseAuth.instance.signOut();
+          save(false);
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => LoginView()));
+        }
+      } catch (signupError) {
+        if (signupError.toString().contains("Given String is empty or null")) {
+        } else if (signupError.toString().contains("ERROR_WRONG_PASSWORD")) {
+          Fluttertoast.showToast(
+            msg: 'The password is invalid',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signupError);
+        } else if (signupError.toString().contains("ERROR_INVALID_EMAIL")) {
+          Fluttertoast.showToast(
+            msg: 'The email is invalid',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signupError);
+        } else if (signupError.toString().contains("ERROR_WEAK_PASSWORD")) {
+          Fluttertoast.showToast(
+            msg: 'The password is too weak',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signupError);
+        } else if (signupError
+            .toString()
+            .contains("ERROR_EMAIL_ALREADY_IN_USE")) {
+          Fluttertoast.showToast(
+            msg: 'The email address is already in use.',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 1,
+          );
+          print(signupError);
+        }
+      }
+    } else {
+      Fluttertoast.showToast(
+          msg: 'Please enter an email.',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 1);
+    }
   }
 
   void load(bool state) {
